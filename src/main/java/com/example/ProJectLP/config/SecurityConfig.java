3ebb9ed619d64cd515d285dfdb0740d01cb12639
projectmiluju <dnjsdyd0712@naver.com>
2024-06-08 05:53:23 +0900

    fix(스프링 시큐리티): 초기 오류 해결
    초기 오류 해결 후 스프링 시큐리티 다시 적용

---
+++ src/main/java/com/example/ProJectLP/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.ProJectLP.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(
+                authorize -> authorize
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .anyRequest().permitAll()
+        );
+        return http.build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled",havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
+    }
+}
