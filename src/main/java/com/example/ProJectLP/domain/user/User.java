a9043bf9090293ae9d04664bcf63cb4c9302b6f4
projectmiluju <dnjsdyd0712@naver.com>
2024-06-08 05:23:57 +0900

    feat(스프링 시큐리티): h2 오류
    h2오류로 일단 변수인 스프링시큐리티 적용 해제

--- src/main/java/com/example/ProJectLP/domain/user/User.java
+++ src/main/java/com/example/ProJectLP/domain/user/User.java
@@ -1,4 +1,20 @@
 package com.example.ProJectLP.domain.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    @Column(nullable = false, length = 10)
+    private String username;
 }
--- src/main/java/com/example/ProJectLP/domain/user/UserRepository.java
+++ src/main/java/com/example/ProJectLP/domain/user/UserRepository.java
@@ -1,4 +1,6 @@
 package com.example.ProJectLP.domain.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
