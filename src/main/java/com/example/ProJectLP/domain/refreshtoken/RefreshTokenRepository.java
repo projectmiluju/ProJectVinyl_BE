2ec09ecabca89bc1717ea5a2cae5d95d6c78690a
projectmiluju <dnjsdyd0712@naver.com>
2024-06-17 15:20:19 +0900

    feat(jwt): jwt token 구현

--- src/main/java/com/example/ProJectLP/domain/refreshtoken/RefreshTokenRepository.java
+++ src/main/java/com/example/ProJectLP/domain/refreshtoken/RefreshTokenRepository.java
@@ -1,4 +1,10 @@
 package com.example.ProJectLP.domain.refreshtoken;
 
-public interface RefreshTokenRepository {
+import com.example.ProJectLP.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByMember(Member member);
 }
