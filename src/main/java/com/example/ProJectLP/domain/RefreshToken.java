f9d0bb22c2b1c7ed0554e5cc896b669468e06790
projectmiluju <dnjsdyd0712@naver.com>
2024-06-17 14:20:01 +0900

    feat(RefreshToken): RefreshToken entity 생성

---
+++ src/main/java/com/example/ProJectLP/domain/RefreshToken.java
@@ -0,0 +1,27 @@
+package com.example.ProJectLP.domain;
+
+import com.example.ProJectLP.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class RefreshToken extends TimeStamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String refreshToken;
+}
