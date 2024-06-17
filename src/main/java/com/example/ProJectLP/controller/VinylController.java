1fb6a14b367d4c194c7c0f743f184ce762721a8e
projectmiluju <dnjsdyd0712@naver.com>
2024-06-17 17:28:25 +0900

    fix(RefreshToken): 토큰으로 member가 확인되지 않는 오류 수정
    sping security 설정 추가

--- src/main/java/com/example/ProJectLP/controller/VinylController.java
+++ src/main/java/com/example/ProJectLP/controller/VinylController.java
@@ -3,15 +3,13 @@ package com.example.ProJectLP.controller;
 import com.example.ProJectLP.dto.request.VinylRequestDto;
 import com.example.ProJectLP.dto.response.ResponseDto;
 import com.example.ProJectLP.service.VinylService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.attribute.UserPrincipal;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,7 +18,7 @@ public class VinylController {
     private final VinylService vinylService;
 
     @RequestMapping(value = "/upload/vinyl", method = RequestMethod.POST)
-    public ResponseDto<?> uploadVinyl(@RequestBody VinylRequestDto requestDto, @AuthenticationPrincipal UserDetails user){
-        return vinylService.uploadVinyl(requestDto,user);
+    public ResponseDto<?> uploadVinyl(@RequestBody VinylRequestDto requestDto, HttpServletRequest request){
+        return vinylService.uploadVinyl(requestDto, request);
     }
 }
--- src/main/java/com/example/ProJectLP/service/VinylService.java
+++ src/main/java/com/example/ProJectLP/service/VinylService.java
@@ -1,35 +1,50 @@
 package com.example.ProJectLP.service;
 
+import com.example.ProJectLP.domain.jwt.TokenProvider;
 import com.example.ProJectLP.domain.member.Member;
-import com.example.ProJectLP.domain.member.MemberRepository;
 import com.example.ProJectLP.domain.vinyl.Vinyl;
 import com.example.ProJectLP.domain.vinyl.VinylRepository;
 import com.example.ProJectLP.dto.request.VinylRequestDto;
 import com.example.ProJectLP.dto.response.ResponseDto;
 import com.example.ProJectLP.dto.response.VinylResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class VinylService {
 
     private final VinylRepository vinylRepository;
-    private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
 
     @Transactional
-    public ResponseDto<?> uploadVinyl(VinylRequestDto requestDto, UserDetails user) {
-        Optional<Member> member = memberRepository.findByUsername(user.getUsername());
+    public ResponseDto<?> uploadVinyl(VinylRequestDto requestDto, HttpServletRequest request) {
+        if (null == request.getHeader("RefreshToken")) {
+            return ResponseDto.fail("400",
+                    "Login is required.");
+        }
 
-        if (member.get().isRole()){
+        if (null == request.getHeader("Authorization")) {
+            return ResponseDto.fail("400",
+                    "Login is required.");
+        }
+
+        Member member = validateMember(request);
+
+
+        if (null == member) {
+            return ResponseDto.fail("400", "INVALID_TOKEN");
+        }
+
+//        if (member.isRole()){
             Vinyl vinyl = Vinyl.builder()
                     .title(requestDto.getTitle())
                     .description(requestDto.getDescription())
                     .artist(requestDto.getArtist())
+                    .genre(requestDto.getGenre())
                     .build();
 
             vinylRepository.save(vinyl);
@@ -40,12 +55,21 @@ public class VinylService {
                             .title(vinyl.getTitle())
                             .description(vinyl.getDescription())
                             .artist(vinyl.getArtist())
+                            .genre(vinyl.getGenre())
                             .createdAt(vinyl.getCreatedAt())
                             .modifiedAt(vinyl.getModifiedAt())
                             .build()
             );
-        }
-        else return ResponseDto.fail("400", "관리자가 아닙니다.");
+//        }
+//        else return ResponseDto.fail("400", "관리자가 아닙니다.");
 
+    }
+
+    @Transactional
+    public Member validateMember(HttpServletRequest request) {
+        if (!tokenProvider.validateToken(request.getHeader("RefreshToken"))) {
+            return null;
+        }
+        return tokenProvider.getMemberFromAuthentication();
     }
 }
