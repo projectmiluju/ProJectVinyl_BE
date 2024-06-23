8dae005ea7d95ac35be587402e7eefabb346a485
projectmiluju <dnjsdyd0712@naver.com>
2024-06-23 23:13:25 +0900

    feat(redis): redis multi database
    refreshtoken 0
    email 1

---
+++ src/main/java/com/example/ProJectLP/config/RedisConfig.java
@@ -0,0 +1,54 @@
+package com.example.ProJectLP.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.a.redis.index}")
+    private int aIndex;
+
+    @Value("${spring.data.b.redis.index}")
+    private int bIndex;
+
+    @Bean
+    @Primary
+    LettuceConnectionFactory connectionFactory1() { return createConnectionFactoryWith(aIndex); }
+
+    @Bean
+    StringRedisTemplate redisTemplate1(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    @Qualifier("2")
+    LettuceConnectionFactory connectionFactory2() { return createConnectionFactoryWith(bIndex); }
+
+    @Bean
+    StringRedisTemplate redisTemplate2(@Qualifier("2") RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    public LettuceConnectionFactory createConnectionFactoryWith(int index) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setDatabase(index);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
