2514ced9f8c562c53e72834623bce94cb198069e
projectmiluju <dnjsdyd0712@naver.com>
2024-06-23 20:29:48 +0900

    feat(이메일 인증): 이메일 인증 기능 테스트 완료
    코드 수정 및 redis index 분리 필요

--- src/main/java/com/example/ProJectLP/config/EmailConfig.java
+++ src/main/java/com/example/ProJectLP/config/EmailConfig.java
@@ -49,22 +49,33 @@ public class EmailConfig {
         mailSender.setUsername(username);
         mailSender.setPassword(password);
         mailSender.setDefaultEncoding("UTF-8");
-        mailSender.setJavaMailProperties(getMailProperties());
 
-        return mailSender;
+        Properties javaMailProperties = new Properties();//JavaMail의 속성을 설정하기 위해 Properties 객체를 생성
+
+        javaMailProperties.put("mail.transport.protocol", "smtp");//프로토콜로 smtp 사용
+        javaMailProperties.put("mail.smtp.auth", "true");//smtp 서버에 인증이 필요
+        javaMailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//SSL 소켓 팩토리 클래스 사용
+        javaMailProperties.put("mail.smtp.starttls.enable", "true");//STARTTLS(TLS를 시작하는 명령)를 사용하여 암호화된 통신을 활성화
+        javaMailProperties.put("mail.debug", "true");//디버깅 정보 출력
+        javaMailProperties.put("mail.smtp.ssl.trust", "smtp.naver.com");//smtp 서버의 ssl 인증서를 신뢰
+        javaMailProperties.put("mail.smtp.ssl.protocols", "TLSv1.2");//사용할 ssl 프로토콜 버젼
+
+        mailSender.setJavaMailProperties(javaMailProperties);//mailSender에 우리가 만든 properties 넣고
+
+        return mailSender;//빈으로 등록한다.
     }
 
-    private Properties getMailProperties() {
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", auth);
-        properties.put("mail.smtp.starttls.enable", starttlsEnable);
-        properties.put("mail.smtp.starttls.required", starttlsRequired);
-        properties.put("mail.smtp.connectiontimeout", connectionTimeout);
-        properties.put("mail.smtp.timeout", timeout);
-        properties.put("mail.smtp.writetimeout", writeTimeout);
-
-        return properties;
-    }
+//    private Properties getMailProperties() {
+//        Properties properties = new Properties();
+//        properties.put("mail.smtp.auth", auth);
+//        properties.put("mail.smtp.starttls.enable", starttlsEnable);
+//        properties.put("mail.smtp.starttls.required", starttlsRequired);
+//        properties.put("mail.smtp.connectiontimeout", connectionTimeout);
+//        properties.put("mail.smtp.timeout", timeout);
+//        properties.put("mail.smtp.writetimeout", writeTimeout);
+//
+//        return properties;
+//    }
 
 
 }
